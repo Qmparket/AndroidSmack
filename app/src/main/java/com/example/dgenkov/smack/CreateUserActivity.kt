e9372d26e2d4f5bf69e8a7126719f50deacbf046
d.genkov <d.genkov@d.genkov>
2018-12-19 11:25:23 +0200

    generate avatar image and background color functionality

--- app/src/main/java/com/example/dgenkov/smack/CreateUserActivity.kt
+++ app/src/main/java/com/example/dgenkov/smack/CreateUserActivity.kt
@@ -1,10 +1,16 @@
 package com.example.dgenkov.smack
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_create_user.*
+import java.util.*
 
 class CreateUserActivity : AppCompatActivity() {
+
+    var userAvatar = "profileDefault"
+    var avatarColor = "[0.5,0.5,0.5,1]"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,9 +19,35 @@ class CreateUserActivity : AppCompatActivity() {
 
     fun generateUserAvatar(view: View) {
 
+        val random = Random()
+        val color = random.nextInt(2)
+        val avatarNumber = random.nextInt(28)
+
+        if(color == 0) {
+            userAvatar = "light$avatarNumber"
+        } else {
+            userAvatar = "dark$avatarNumber"
+
+        }
+
+        val resourceId = resources.getIdentifier(userAvatar, "drawable", packageName)
+        createAvatarImageView.setImageResource(resourceId)
     }
 
     fun generateColorClicked(view: View) {
+        val random = Random()
+        val red = random.nextInt(255)
+        val green = random.nextInt(255)
+        val blue = random.nextInt(255)
+
+        createAvatarImageView.setBackgroundColor(Color.rgb(red,green,blue))
+
+        val savedR = red.toDouble() / 255
+        val savedG = green.toDouble() / 255
+        val savedB = blue.toDouble() / 255
+
+        avatarColor = "[$savedR, $savedG, $savedB, 1]"
+        println("@avatarColor $avatarColor")
 
     }
 
