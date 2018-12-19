71e99d6413ba6662199605f69fbb31d85a331061
d.genkov <d.genkov@d.genkov>
2018-12-19 11:08:37 +0200

    added the create layout and the login layout

---
+++ app/src/main/java/com/example/dgenkov/smack/LoginActivity.kt
@@ -0,0 +1,23 @@
+package com.example.dgenkov.smack
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun onLoginButtonClicked(view: View) {
+
+    }
+
+    fun onSignupButtonClicked(view: View) {
+        val signupIntent = Intent(this, CreateUserActivity::class.java)
+        startActivity(signupIntent)
+    }
+}
